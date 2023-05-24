6507b3f0d87ea162fa1e142d0a5fa5396aa59ecf
Kalawskyy <93537482+Kalawskyy@users.noreply.github.com>
2023-05-24 21:50:24 +0200

    Kalkulator ver: 1.0 | Sprawy Kalkulator

--- src/Components/ButtonsPanel.java
+++ src/Components/ButtonsPanel.java
@@ -10,13 +10,12 @@ import java.util.Objects;
 
 public class ButtonsPanel extends JPanel implements ActionListener {
     private final String[] nameButton = {"C_line", "C_all", "√", "%", "MRC", "M-", "M+", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "Un/Lock", "0", ".", "="};
-    private final JLabel inLabel,outLabel;
+    private final JLabel inLabel, outLabel;
     private final String[] lines;
     private final Boolean[] blines;
-
+    private String memory;
     private Boolean lockKB;
-
-    ButtonsPanel(JLabel inLabel,JLabel outLabel, Boolean lockKB, String[] lines, Boolean[] blines) {
+    ButtonsPanel(JLabel inLabel, JLabel outLabel, Boolean lockKB, String[] lines, Boolean[] blines) {
         this.setBackground(Color.darkGray);
         this.setBounds(0, 100, 400, 500);
         this.setLayout(new GridLayout(6, 4, 15, 15));
@@ -26,15 +25,14 @@ public class ButtonsPanel extends JPanel implements ActionListener {
         this.lockKB = lockKB;
         this.lines = lines;
         this.blines = blines;
-        this.outLabel=outLabel;
+        this.outLabel = outLabel;
+        this.memory="0";
     }
-
     private void addComponents() {
         for (String x : nameButton) {
             this.add(buttonCrate(x));
         }
     }
-
     private JButton buttonCrate(String name) {
         JButton t = new JButton(name);
         t.addActionListener(this);
@@ -42,7 +40,6 @@ public class ButtonsPanel extends JPanel implements ActionListener {
         t.setBackground(Color.lightGray);
         return t;
     }
-
     @Override
     public void actionPerformed(ActionEvent e) {
         String cmd = e.getActionCommand();
@@ -51,22 +48,17 @@ public class ButtonsPanel extends JPanel implements ActionListener {
         }
         for (String a : nameButton) {
             if (cmd.equals(a)) {
-                System.out.println("Cynamonek");
                 inLabel.setText(correctInput(inLabel.getText(), a));
             }
         }
-
-
     }
-
     String correctInput(String inputLine, String newCharacter) {
         boolean point = false;
         try {
             for (int i = 0; i < inputLine.length(); i++) {
-                boolean b = !point;
-                System.out.println(inputLine.charAt(i));
-                if (inputLine.charAt(i) == '.' && b) {
+                if (inputLine.charAt(i) == '.') {
                     point = true;
+                    break;
                 }
             }
             if ((inputLine.length() >= 30 || (point && Objects.equals(newCharacter, ".")) || (!point && inputLine.length() == 0 && Objects.equals(newCharacter, "."))) && !blines[1]) {
@@ -74,8 +66,26 @@ public class ButtonsPanel extends JPanel implements ActionListener {
             } else if (newCharacter.equals("C_line")) {
                 System.out.println("c_line");
                 return "";
+            }else if (newCharacter.equals("C_all")) {
+                System.out.println("C_all");
+                clearData();
+                return "";
+            }else if ((newCharacter.equals("MRC"))||(newCharacter.equals("M+"))||(newCharacter.equals("M-"))) {
+                if (newCharacter.equals("MRC")){
+                    String temp=memory;
+                    this.memory="0";
+                    return correctInput(temp,"");
+                } else if (newCharacter.equals("M+")) {
+                  Double temp=Double.parseDouble(memory)+Double.parseDouble(outLabel.getText());
+                  this.memory= String.valueOf(temp);
+                  return inputLine;
+                }else {
+                    Double temp=Double.parseDouble(memory)-Double.parseDouble(outLabel.getText());
+                    this.memory= String.valueOf(temp);
+                    return inputLine;
+                }
             } else if (newCharacter.equals("*") || newCharacter.equals("/") || newCharacter.equals("-") || newCharacter.equals("+") || newCharacter.equals("√") || newCharacter.equals("%")) {
-                if (!blines[1]) {
+                if ((!blines[1])&&!inputLine.isBlank()){
                     if (!blines[0]) {
                         blines[0] = true;
                         lines[0] = inputLine;
@@ -87,27 +97,32 @@ public class ButtonsPanel extends JPanel implements ActionListener {
                 }
                 return inputLine;
             } else if (newCharacter.equals("=")) {
-                lines[2]=inputLine;
-                MathLineInterpreter tMath= new MathLineInterpreter();
-                 outLabel.setText(tMath.result(lines));
-
+                if (lines[0].isEmpty()){
+                    lines[0]=inputLine;
+                }
+                lines[2] = inputLine;
+                MathLineInterpreter tMath = new MathLineInterpreter();
+                outLabel.setText(tMath.result(lines));
+                clearData();
                 return "";
             } else {
                 if (blines[0] && !blines[1]) {
                     blines[1] = true;
                     inputLine = "";
                     return inputLine + newCharacter;
-                }else{
-                    System.out.println("Kevin homo");
-                    return inputLine+newCharacter ;
+                } else {
+                    return inputLine + newCharacter;
                 }
-
             }
-
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("Kevin homo1");
             return inputLine;
+        }
+    }
+    private void clearData(){
+        for (int i = 0;i<3; i++){
+            lines[i]="";
+            blines[i]=false;
         }
     }
 }
--- src/Components/Math/MathLineInterpreter.java
+++ src/Components/Math/MathLineInterpreter.java
@@ -5,15 +5,31 @@ public class MathLineInterpreter {
     public String result(String lines[]) {
         this.lines=lines;
         Double number1=Double.valueOf(lines[0]);
-
-        if (lines[1].length()>1&&"**".equals((String.valueOf(lines[1].charAt(lines[1].length() - 1))+String.valueOf(lines[1].charAt(lines[1].length() - 2))))){
+        if (lines[1].isEmpty()){
+            return (String.valueOf(number1));
+        } else if (lines[1].length()>1&&"**".equals((String.valueOf(lines[1].charAt(lines[1].length() - 1))+String.valueOf(lines[1].charAt(lines[1].length() - 2))))){
              return(String.valueOf(number1*number1));
         } else if ("*".equals(String.valueOf(lines[1].charAt(lines[1].length() - 1)))) {
             Double number2=Double.valueOf(lines[2]);
             return  String.valueOf(number1*number2);
+        } else if ("/".equals(String.valueOf(lines[1].charAt(lines[1].length() - 1)))) {
+            Double number2=Double.valueOf(lines[2]);
+            if (number2==0){
+                return "Błąd";
+            }
+            return  String.valueOf(number1/number2);
+        }else if ("+".equals(String.valueOf(lines[1].charAt(lines[1].length() - 1)))) {
+            Double number2=Double.valueOf(lines[2]);
+            return  String.valueOf(number1+number2);
+        }else if ("-".equals(String.valueOf(lines[1].charAt(lines[1].length() - 1)))) {
+            Double number2=Double.valueOf(lines[2]);
+            return  String.valueOf(number1-number2);
+        }else if ("√".equals(String.valueOf(lines[1].charAt(lines[1].length() - 1)))) {
+            return  String.valueOf(Math.sqrt(number1));
+        }else if ("%".equals(String.valueOf(lines[1].charAt(lines[1].length() - 1)))) {
+            Double number2=Double.valueOf(lines[2]);
+            return  String.valueOf((number1/100)*number2);
         }
-        String result ="funging";
-
-        return result;
+        return"";
     }
 }
